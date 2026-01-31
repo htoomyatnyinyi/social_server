6e628606b466c078063271883511caa5f8a91f72
htoomyatnyinyi <htoomyatnyinyi@gmail.com>
2026-01-31 17:52:29 +0700

    enable websocket plugin in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { staticPlugin } from "@elysiajs/static";
+import { websocket } from "@elysiajs/websocket";
 import { authRoutes } from "./routes/auth";
 import { postRoutes } from "./routes/posts";
 import { chatRoutes } from "./routes/chat";
@@ -8,10 +9,12 @@ import { chatRoutes } from "./routes/chat";
 const app = new Elysia()
   .use(cors())
   .use(staticPlugin())
+  .use(websocket())
   .get("/", () => ({ message: "Social App API is running" }))
   .use(authRoutes)
   .use(postRoutes)
   .use(chatRoutes)
+
   .listen(process.env.PORT || 8080);
 
 console.log(
