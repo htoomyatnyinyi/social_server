aa6ad7ecf0ebf662ab39495751e85208938853ac
htoomyatnyinyi <htoomyatnyinyi@gmail.com>
2026-02-03 14:07:42 +0700

    clear un-necessary code

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,3 @@
-// import { Elysia } from "elysia";
-// import { cors } from "@elysiajs/cors";
-// import { staticPlugin } from "@elysiajs/static";
-// import { websocket } from "@elysiajs/websocket";
-// import { authRoutes } from "./routes/auth";
-// import { postRoutes } from "./routes/posts";
-// import { chatRoutes } from "./routes/chat";
-
-// const app = new Elysia()
-//   .use(cors())
-//   .use(staticPlugin())
-//   .use(websocket())
-//   .get("/", () => ({ message: "Social App API is running" }))
-//   .use(authRoutes)
-//   .use(postRoutes)
-//   .use(chatRoutes)
-
-//   .listen(process.env.PORT || 8080);
-
-// console.log(
-//   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-// );
-
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { staticPlugin } from "@elysiajs/static";
